dc98f82c645311743e7df9db2bd0a15ef39c07bc
Pedro Gomes <pegom0896@gmail.com>
2017-11-28 23:55:45 +0100

    Done till exercice2

---
+++ Lab 8/src/exercise1/BMP.java
@@ -0,0 +1,19 @@
+package lu.uni.programming1.lab8.exercise1;
+
+public class BMP extends ImageFile {
+
+	private int colordepth;
+
+	public BMP(String name, int width, int height, int colordepth) {
+		super(name, width, height);
+		this.colordepth = colordepth;
+	}
+
+	public void setColordepth(int colordepth) {
+		this.colordepth = colordepth;
+	}
+
+	public int getColordepth() {
+		return this.colordepth;
+	}
+}
---
+++ Lab 8/src/exercise1/ImageFile.java
@@ -0,0 +1,21 @@
+package lu.uni.programming1.lab8.exercise1;
+
+public class ImageFile extends Item {
+
+	private int width;
+	private int height;
+
+	public ImageFile(String name, int width, int height) {
+		super(name);
+		this.width = width;
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return this.width;
+	}
+
+	public int getHeight() {
+		return this.height;
+	}
+}
